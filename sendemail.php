2df8fd533d026eb89de969c3ed6a2fc11cb075ae
luckydhandy <luckydhandy791991@gmail.com>
2018-06-24 01:16:58 +0700

    2

--- sendemail.php
+++ sendemail.php
@@ -1,13 +1,17 @@
 <?php
 /* tentukan email penerima */
-$myemail  = "aaronabbyla@gmail.com";
+$myemail  = "luckydhandy791991@gmail.com";
 /* Mengecek semua kolom input menggunakan  function check_input */
-$name = check_input($_POST['name'], "Masukkan nama anda");
-$spouse  = check_input($_POST['spouse'], "Masukan nama pasangan anda");
-$and1    = check_input($_POST['and1']);
-$and2  = check_input($_POST['and2']);
-$category  = check_input($_POST['category']);
-$message= check_input($_POST['message'], "Message for Groom & Bride");
+$yourname = check_input($_POST['name'], "Masukkan nama anda");
+$email  = check_input($_POST['email'], "Tulis subject");
+$phone    = check_input($_POST['phone']);
+$whatsapp  = check_input($_POST['whatsapp']);
+$ukuranrumahtanah  = check_input($_POST['ukuranrumahtanah']);
+$jumlahkamar  = check_input($_POST['jumlahkamar']);
+$dinding  = check_input($_POST['dinding']);
+$dapurdindingbelakang  = check_input($_POST['dapurdindingbelakang']);
+$pagar = check_input($_POST['pagar']);
+$message= check_input($_POST['message'], "Tuliskan pertanyaan atau komentar anda");
 /* Jika e-mail tidak valid, tampilkan pesan error */
 if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email))
 {
@@ -19,20 +23,25 @@ if (!preg_match("/^(https?:\/\/+[\w\-]+\.[\w\-]+)/i", $website))
     $website = '';
 }
 /* Isi pesan di kotak masuk e-mail anda */
-$message = "Hallo kami akan datang!
+$message = "Hallo..,!
 Ada pesan baru dari contact form:
-Name: $name
-Spouse: $spouse
-And: $and1
-And: $and2
-Category: $category
-Message:
+Nama: $yourname
+Phone: $phone
+E-mail: $email
+WhatsApp: $whatsapp
+Ukuran Rumah / Tanah: $ukuranrumahtanah
+Jumlah Kamar: $jumlahkamar
+Dinding: $dinding
+Dapur dan Dinding Belakang: $dapurdindingbelakang
+Pagar: $pagar
+
+Pertanyaan/Komentar:
 $message
 ";
 /* Mengrim pesan menggunakan mail() function */
 mail($myemail, $subject, $message);
 /* Alihkan pengunjung ke halaman sukses */
-header('Location: thankyou.html');
+header('Location: Success.html');
 exit();
 /* Functions yg kita gunakan */
 function check_input($data, $problem='')
@@ -52,7 +61,7 @@ function show_error($myError)
     <html>
     <body>
     <b>Eror! Tolong lengkapi formulir pemesanan anda.</b><br />
-    <?php echo $myError; ?><a href="form.html"> Kembali ke form</a>
+    <?php echo $myError; ?><a href="Contactus.html"> Kembali ke form</a>
     </body>
     </html>
 <?php
